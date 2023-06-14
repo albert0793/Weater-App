d746458043244408bb3ccdfc95e1e2ca2566b12e
albert0793 <carlos.guzman93@live.com>
2023-06-13 19:16:54 -0500

    la pagina es 100% dinamica

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const errorBox = document.querySelector(".error__box");
 const temperatureElement = document.querySelector("#temperature");
 const windElement = document.querySelector("#wind");
 const humidityElement = document.querySelector("#humidity");
+const image = document.querySelector("#station__image");
 async function getWeatherInfo() {
   const cityName = searcHBar.value;
   const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`;
@@ -20,18 +21,22 @@ async function getWeatherInfo() {
           alert("Please wrote a city name before.");
         }, 500);
       } else if (data.cod == 404) {
-        container.style.height = "380px";
+        container.style.height = "350px";
         errorBox.style.display = "flex";
+        weatherContainer.innerHTML = "";
         weatherContainer.style.display = "none";
+        return;
       } else {
         const { humidity, temp } = data.main;
         const wind = data.wind.speed;
         const city = data.name;
+        const imgUrl =  'https://openweathermap.org/img/wn';
         temperatureElement.textContent = `${convertWeatherDegrees(temp)}°C`;
+        image.src = `${imgUrl}/${data.weather[0].icon}.png`;
         windElement.textContent = `${wind}Km/h`;
         humidityElement.textContent = `${humidity}%`;
         cityElement.textContent = `${city}`;
-        container.style.height = "550px";
+        container.style.height = "350px";
         errorBox.style.display = "none";
         weatherContainer.style.display = "flex";
       }
@@ -39,12 +44,19 @@ async function getWeatherInfo() {
     });
 }
 
-
-function convertWeatherDegrees(degrees) {
-    return Math.floor((degrees - 32) / 1.8);
+function selectImage(temp) {
+  if (temp <= 10) {
+    return "winter.png";
+  } else if (temp < 20) {
+    return "rain.jpeg";
+  } else {
+    return "sun.png";
+  }
 }
 
-console.log(convertWeatherDegrees(328.4));
+function convertWeatherDegrees(degrees) {
+  return Math.floor((degrees - 32) / 1.8);
+}
 
 searchButton.addEventListener("click", getWeatherInfo);
 window.addEventListener("keydown", async (e) => {
